b4df1f63cdbe259af17de38e904042bb40e8e209
Etienne Carriere <etienne.carriere@linaro.org>
2019-08-13 09:39:02 +0200

    build: enhance entry guards in internal header files
    Add prefix INTERNAL_ to defined macro for internal header file.
    This change prevents an internal/foo.h from obscuring generic
    foo.h because former and later both guards from
    
    #ifndef FOO_H
    #define FOO_H
    (...)
    #endif /*FOO_H*/
    
    As internal/*.h are reacheable from #include <internal/*.h>, INTERNAL_
    looks a generic enough prefix.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- module/scmi/include/internal/scmi_base.h
+++ module/scmi/include/internal/scmi_base.h
@@ -8,8 +8,8 @@
  *      SCMI base protocol definitions.
  */
 
-#ifndef SCMI_BASE_H
-#define SCMI_BASE_H
+#ifndef INTERNAL_SCMI_BASE_H
+#define INTERNAL_SCMI_BASE_H
 
 #define SCMI_PROTOCOL_ID_BASE      UINT32_C(0x10)
 #define SCMI_PROTOCOL_VERSION_BASE UINT32_C(0x10000)
@@ -87,4 +87,4 @@ struct __attribute((packed)) scmi_base_discover_agent_p2a {
 
 extern struct scp_scmi_protocol scmi_base_protocol;
 
-#endif /* SCMI_BASE_H */
+#endif /* INTERNAL_SCMI_BASE_H */
